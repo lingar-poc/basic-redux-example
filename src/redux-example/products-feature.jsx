90bfd766fe13505885e0b1e274996c967ffabd9c
lingar <yimprogramming@gmail.com>
2021-02-07 11:32:50 +0200

    Adding most-expensive-product view

--- src/redux-example/products-feature.jsx
+++ src/redux-example/products-feature.jsx
@@ -46,7 +46,7 @@ export function ProductsDisplayRedux(props) {
         interval = setInterval(() => {
                 //instead of doing like in regular component:
                 // this.setState({
-                 //     book: this.getRandomInt(100),
+                //     book: this.getRandomInt(100),
                 //     bread: this.getRandomInt(100),
                 //     shoes: this.getRandomInt(100),
                 //     candle: this.getRandomInt(100)
@@ -65,6 +65,7 @@ export function ProductsDisplayRedux(props) {
             }
             , seconds * 3000);
     }
+
     //for generate random value below 100
     function getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
@@ -81,11 +82,50 @@ export function ProductsDisplayRedux(props) {
 
     //invocations
     dataToArray();
-    console.log("data = ",data);
+    console.log("data = ", data);
     console.table(data);
     return (
         <div>
             <ProductsList data={data}/>
+            <MostExpensiveRedux/>
         </div>
     )
+}
+
+/**
+ * View for seeing the current most expensive product
+
+ */
+function MostExpensiveRedux() {
+    const productsState = useSelector(selectProducts);
+    let data = [];
+    dataToArray();
+    console.log("data = ", data);
+
+    function dataToArray() {
+        data = [];
+        for (const [key, value] of Object.entries(productsState)) {
+            console.log(`${key}: ${value}`);
+            data.push({name: key, price: value});
+        }
+    }
+
+    function max() {
+        let expensiveItem = {item: "", price: 0};
+        for (let i of data) {
+            console.log("i = ", i);
+            if (i.price > expensiveItem.price) {
+                expensiveItem = i;
+            }
+        }
+        return expensiveItem;
+    }
+
+    return (<div className="most-expensive">
+            <h4>Expensive: {max().name} <br/>
+                {max().price}</h4>
+
+        </div>
+    )
+
 }
